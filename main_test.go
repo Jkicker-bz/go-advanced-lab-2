66a982ed99ae6402d1cace935753e1f9f937f902
Antonio Itza <2024161195@ub.edu.bz>
2026-01-31 18:22:43 +0000

    Part-1 complete: Table-driven tests

---
+++ main_test.go
@@ -0,0 +1,89 @@
+package main
+
+import "testing"
+
+func TestFactorial(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   int
+		want    int
+		wantErr bool
+	}{
+		{name: "factorial of 0", input: 0, want: 1, wantErr: false},
+		{name: "factorial of 1", input: 1, want: 1, wantErr: false},
+		{name: "factorial of 3", input: 3, want: 6, wantErr: false},
+		{name: "factorial of 5", input: 5, want: 120, wantErr: false},
+		{name: "factorial of 10", input: 10, want: 3628800, wantErr: false},
+		{name: "factorial of negative number", input: -4, want: 0, wantErr: true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Factorial(tt.input)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Factorial() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Factorial() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPrime(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   int
+		want    bool
+		wantErr bool
+	}{
+		{name: "Prime number: 2", input: 2, want: true, wantErr: false},
+		{name: "Prime number: 3", input: 3, want: true, wantErr: false},
+		{name: "Composite number: 6", input: 6, want: false, wantErr: true},
+		{name: "Number less than 2", input: 1, want: false, wantErr: true},
+		{name: "Negative number", input: -5, want: false, wantErr: true},
+		{name: "Large prime number: 29", input: 29, want: true, wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := IsPrime(tt.input)
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("IsPrime() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("IsPrime() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPower(t *testing.T) {
+	tests := []struct {
+		name     string
+		base     int
+		exponent int
+		want     int
+		wantErr  bool
+	}{
+		{name: "Positive exponent", base: 2, exponent: 3, want: 8, wantErr: false},
+		{name: "Exponent zero", base: 5, exponent: 0, want: 1, wantErr: false},
+		{name: "Base zero", base: 0, exponent: 2, want: 0, wantErr: false},
+		{name: "Exponent equals 1", base: 25, exponent: 1, want: 25, wantErr: false},
+		{name: "Negative exponent", base: 2, exponent: -3, want: 0, wantErr: true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Power(tt.base, tt.exponent)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Power() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Power() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
