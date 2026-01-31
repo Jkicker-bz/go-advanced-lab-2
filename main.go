5772c299bb027cc6ccf319c1288e612a694ef6e6
Antonio Itza <2024161195@ub.edu.bz>
2026-01-31 18:53:26 +0000

    Added funcs and main test code

--- main.go
+++ main.go
@@ -46,6 +46,53 @@ func Power(base, exponent int) (int, error) {
 	return result, nil
 }
 
-func main() {
+func MakeCounter(start int) func() int {
+	count := start
+	return func() int {
+		count++
+		return count
+	}
+}
 
+func MakeMultiplier(factor int) func(int) int {
+	return func(x int) int {
+		return x * factor
+	}
+}
+
+func MakeAccumulator(initial int) (add func(int), subtract func(int), get func() int) {
+	total := initial
+	add = func(x int) {
+		total += x
+	}
+	subtract = func(x int) {
+		total -= x
+	}
+	get = func() int {
+		return total
+	}
+	return add, subtract, get
+}
+func main() {
+	// --- Testing MakeCounter ---
+	counter1 := MakeCounter(0)
+	fmt.Println(counter1()) // 1
+	fmt.Println(counter1()) // 2
+
+	counter2 := MakeCounter(10)
+	fmt.Println(counter2()) // 11
+	fmt.Println(counter1()) // 3 (Independent from counter2)
+
+	// --- Testing MakeMultiplier ---
+	double := MakeMultiplier(2)
+	triple := MakeMultiplier(3)
+	fmt.Println(double(5)) // 10
+	fmt.Println(triple(5)) // 15
+
+	// --- Testing MakeAccumulator ---
+	add, sub, get := MakeAccumulator(100)
+	add(50)
+	fmt.Println(get()) // 150
+	sub(20)
+	fmt.Println(get()) // 130
 }
